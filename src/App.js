3f5e02071dad10cfc26bdbfac886a902bf400381
Vágó Rebeka <rebeka.vago19@gmail.com>
2023-01-14 20:44:38 +0100

    szakaszok kialakítva

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Teendők</h1>
+      <section>
+        <h2>Feladat hozzáadása</h2>
+      </section>
+      <section>
+        <h2>Teendők</h2>
+      </section>
     </div>
   );
 }
