bc399cd0e11e7dc8103c169dd0b49e0e3ccaef84
Vágó Rebeka <rebeka.vago19@gmail.com>
2023-01-14 20:56:34 +0100

    teendők megjelenítése

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import './App.css';
 
 function App() {
+  const teendok = [];
+  const teendoLista = [];
+  teendok.forEach(teendo=>{
+    teendoLista.push(<div>
+      <p><input type="checkbox"/>{teendo}</p>
+    </div>)
+  })
   return (
-    <div>
+    <main>
       <h1>Teendők</h1>
       <section>
         <h2>Feladat hozzáadása</h2>
       </section>
       <section>
         <h2>Teendők</h2>
+        {teendoLista}
       </section>
-    </div>
+    </main>
   );
 }
 
