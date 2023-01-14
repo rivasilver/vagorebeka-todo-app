16e23a4b70e704544ffdf1ebc5bc606fa2207033
Vágó Rebeka <rebeka.vago19@gmail.com>
2023-01-14 21:11:57 +0100

    űrlap kinézete

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import './App.css';
 
 function App() {
   const teendok = [];
+  const teendo = "";
   const teendoLista = [];
+  const teendoHozzaadasa = ()=>{
+
+  }
   teendok.forEach(teendo=>{
     teendoLista.push(<div>
       <p><input type="checkbox"/>{teendo}</p>
@@ -13,13 +17,16 @@ function App() {
       <h1>Teendők</h1>
       <section>
         <h2>Feladat hozzáadása</h2>
+        <label for="input">Feladat:</label><br/>
+        <input id="input" type="text" placeholder="Feladat" value={teendo}/><br/>
+        <button onClick={()=>teendoHozzaadasa()}>Hozzáad</button>
       </section>
       <section>
         <h2>Teendők</h2>
         {teendoLista}
       </section>
     </main>
-  );
+  ); 
 }
 
 export default App;
