12c7dff9510e0341b9f8ed9867ac6cf4c8052457
Vágó Rebeka <rebeka.vago19@gmail.com>
2023-01-14 22:29:41 +0100

    űrlap elküldése

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const teendok = [];
-  const teendo = "";
+  const [teendok, setTeendok] = useState([]);
+  const [teendo, setTeendo] = useState("");
   const teendoLista = [];
-  const teendoHozzaadasa = ()=>{
 
+  const teendoHozzaadasa = ()=>{
+    let teendoHossz = teendo.length;
+    if (teendoHossz === 0){
+      alert("Nem lett feladat megadva!")
+    }
+    else if (teendok.includes(teendo)){
+      alert("A feladat már szerepel a listában!")
+    }
+    else{
+      const ujTeendok = [];
+      teendok.forEach(teendo=>ujTeendok.push(teendo));
+      ujTeendok.push(teendo);
+      setTeendok(ujTeendok);
+      setTeendo("");
+    }
   }
+
   teendok.forEach(teendo=>{
     teendoLista.push(<div>
       <p><input type="checkbox"/>{teendo}</p>
     </div>)
   })
+
   return (
     <main>
       <h1>Teendők</h1>
       <section>
         <h2>Feladat hozzáadása</h2>
         <label for="input">Feladat:</label><br/>
-        <input id="input" type="text" placeholder="Feladat" value={teendo}/><br/>
+        <input id="input" type="text" placeholder="Feladat" value={teendo} onInput={e=>setTeendo(e.target.value)}/><br/>
         <button onClick={()=>teendoHozzaadasa()}>Hozzáad</button>
       </section>
       <section>
